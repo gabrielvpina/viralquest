f75827be8910fdcb922d34a79e1f838f1b9e21f5
gabrielvpina <gvpina.rodrigues@gmail.com>
2026-04-09 22:41:04 -0300

    restart: refactor of all project with pixi

---
+++ main.py
@@ -0,0 +1,11 @@
+from viralquest.input_parser import FastaParser
+
+meu_parser = FastaParser("data/meu_dna.fasta")
+meu_parser.read_input_file()
+
+primeira_seq = meu_parser.sequences[0]
+
+print(f"ID: {primeira_seq.id}")
+print(f"Tamanho: {primeira_seq.length} bp")
+print(f"Bases N: {primeira_seq.n_count}")
+print(f"Conteúdo GC: {primeira_seq.gc_content:.2f}%")
